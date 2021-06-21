76906e363c1df17be505be5430ad5ac58eab7a94
KANISHKA NIM <67909021+kanishkanim@users.noreply.github.com>
2021-06-21 16:10:09 +0530

    Added solution - LeetHub

---
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String input_line[] = in.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.matrixMultiplication(N, arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+
+    // Matrix Ai has dimension p[i-1] x p[i] for i = 1..n
+    static int matrixMultiplication(int n, int arr[])
+    {
+        /* For simplicity of the 
+        program, one extra row and
+        one extra column are allocated in m[][].  0th row
+        and 0th column of m[][] are not used */
+        int m[][] = new int[n][n];
+
+        int i, j, k, L, q;
+
+        /* m[i, j] = Minimum number of scalar
+        multiplications needed to compute the matrix
+        A[i]A[i+1]...A[j] = A[i..j] where
+        dimension of A[i] is p[i-1] x p[i] */
+
+        // cost is zero when multiplying one matrix.
+        for (i = 1; i < n; i++)
+            m[i][i] = 0;
+
+        // L is chain length.
+        for (L = 2; L < n; L++) 
+        {
+            for (i = 1; i < n - L + 1; i++) 
+            {
+                j = i + L - 1;
+                if (j == n)
+                    continue;
+                m[i][j] = Integer.MAX_VALUE;
+                for (k = i; k <= j - 1; k++) 
+                {
+                    // q = cost/scalar multiplications
+                    q = m[i][k] + m[k + 1][j]
+                        + arr[i - 1] * arr[k] * arr[j];
+                    if (q < m[i][j])
+                        m[i][j] = q;
+                }
+            }
+        }
+
+        return m[1][n - 1];
+    }
+}
