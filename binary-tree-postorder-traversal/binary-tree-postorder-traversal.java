21284f38ca03890d13c897a60939045f22bdcc91
KANISHKA NIM <67909021+kanishkanim@users.noreply.github.com>
2021-06-23 01:50:01 +0530

    Time: 1 ms (15.75%), Space: 37.2 MB (69.10%) - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,16 +14,48 @@
  * }
  */
 
-// Recursive
+// Iterative Approach
 
 class Solution {
-
-    List<Integer> ans = new ArrayList<Integer>();
+    public static class Pair{
+        TreeNode node;
+        int state;
+        
+        Pair(TreeNode node, int state){
+            this.node = node;
+            this.state = state;
+        }
+    }
     public List<Integer> postorderTraversal(TreeNode root) {
-        if (root == null) return ans;
-        postorderTraversal(root.left);
-        postorderTraversal(root.right);
-        ans.add(root.val);
-        return ans;
+       if(root == null) return new ArrayList<>();
+        
+       Stack<Pair> st = new Stack<>();
+       st.push(new Pair(root, 1));
+       List<Integer> postorder = new ArrayList<>();
+       
+       while(st.size() > 0){
+           Pair top =  st.peek();
+           
+           if(top.state == 1){
+               //PRE ORDER AREA
+               top.state++;
+               if(top.node.left != null){
+                   st.push(new Pair(top.node.left, 1));
+               }
+           }
+           else if(top.state == 2){
+               //IN ORDER AREA
+               top.state++;
+               if(top.node.right != null){
+                   st.push(new Pair(top.node.right, 1));
+               }
+           }
+           else if(top.state == 3){
+               //POST ORDER AREA
+               postorder.add(top.node.val);
+               st.pop();
+           }
+       }
+        return postorder;
     }
 }
