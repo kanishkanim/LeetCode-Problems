cc4381c78b4bc097755da2ebffb31b8a6d15540f
KANISHKA NIM <67909021+kanishkanim@users.noreply.github.com>
2021-08-07 18:45:07 +0530

    Time: 5 ms (23.95%), Space: 39.4 MB (56.13%) - LeetHub

--- missing-number/missing-number.java
+++ missing-number/missing-number.java
@@ -1,14 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
             map.put(nums[i], 1);
+        }
         
-        for(int i = 0; i < map.size(); i++)
-        {
-            //then that number is missing
-            if(!map.containsKey(i + 1)) return i + 1;   
+        for(int i = 0; i <= nums.length; i++){
+            if(map.get(i) == null)
+                return i;
         }
         return 0;
     }
