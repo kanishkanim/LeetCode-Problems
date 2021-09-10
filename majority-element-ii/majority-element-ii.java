153ffa797bb5cfbbf4c4b18c45d8fcb62dbba1a4
KANISHKA NIM <67909021+kanishkanim@users.noreply.github.com>
2021-09-11 01:42:28 +0530

    Time: 2 ms (69.92%), Space: 46.7 MB (9.70%) - LeetHub

--- majority-element-ii/majority-element-ii.java
+++ majority-element-ii/majority-element-ii.java
@@ -1,41 +1,50 @@
 class Solution {
-
-public List<Integer> majorityElement(int[] nums) {
-    List<Integer> result = new ArrayList<>();
-    if(nums.length==0)
-        return result;
-    int count1 = 0, count2 = 0;
-    int num1 = nums[0], num2 = nums[0];
-    for (int num : nums) {
-        if (num1 == num)
-            count1++;
-        else if (num2 == num)
-            count2++;
-        else if (count1 == 0) {
-            num1 = num;
-            count1++;
-        } else if (count2 == 0) {
-            num2 = num;
-            count2++;
-        } else {
-            count1--;
-            count2--;
+    
+    public static boolean isMajority(int[] nums, int val) {
+        int count = 0;
+        
+        for(int ele : nums) {
+            if(ele == val)
+                count++;
         }
+        
+        return count > nums.length / 3;
     }
-    count1 = 0;
-    count2 = 0;
-    for (int num : nums) {
-        if (num1 == num)
-            count1++;
-        else if (num2 == num)
-            count2++;
+    
+    public List<Integer> majorityElement(int[] nums) {
+        int val1 = nums[0];
+        int count1 = 1;
+        int val2 = nums[0];
+        int count2 = 0;
+        
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == val1){
+                count1++;
+            }else if(nums[i] == val2){
+                count2++;
+            }else{
+                // might be a triplet or a fresh start
+                if(count1 == 0){
+                    val1 = nums[i];
+                    count1 = 1;
+                }else if(count2 == 0){
+                    val2 = nums[i];
+                    count2 = 1;
+                }else{
+                    count1--;
+                    count2--;
+                }
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        
+        //checking if val1 and val2 are present in majority or not
+        if(isMajority(nums, val1)){
+            res.add(val1);
+        }
+        if(val1 != val2 && isMajority(nums, val2) == true){
+            res.add(val2);
+        }
+        return res;
     }
-    if (count1 > nums.length / 3) {
-        result.add(num1);
-    }
-    if (count2 > nums.length / 3) {
-        result.add(num2);
-    }
-    return result;
-}
 }
