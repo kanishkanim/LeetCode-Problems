87402da6c9e94dcfa9f51879c60fbb28e8265c5b
KANISHKA NIM <67909021+kanishkanim@users.noreply.github.com>
2021-09-10 21:38:29 +0530

    Time: 1 ms (99.89%), Space: 44.9 MB (42.24%) - LeetHub

--- majority-element/majority-element.java
+++ majority-element/majority-element.java
@@ -1,38 +1,21 @@
 class Solution {
-    
-    public static int validCandidate(int[] nums){
+    public int majorityElement(int[] nums) {
         int val = nums[0];
         int count = 1;
         
         for(int i = 1; i < nums.length; i++){
-            if(val == nums[i]){
-                count++; //same element --> increment in freq
+            if(nums[i] == val){
+                count++;
             }else{
-                count--; //distinct element --> map it with val
-            }
-            
-            if(count == 0){
-                val = nums[i];
-                count = 1;
+                if(count > 0){
+                    count--; //pairing of distinct elements
+                }else{
+                    //reset
+                    val = nums[i];
+                    count = 1;
+                }
             }
         }
         return val;
-    }
-    
-    public int majorityElement(int[] nums) {
-        int value = validCandidate(nums);
-        
-        int count = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == value){
-                count++;
-            }
-            
-            if(count > nums.length / 2){
-                continue;
-            }
-            
-        }
-        return value;
     }
 }
